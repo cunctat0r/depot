e810e4b2370d978af2033319ae4e04e092d070ca
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-28 12:41:02 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,13 @@ Product.create!(title: 'Espresso',
   description: %{Coffee brewed by forcing a small amount of nearly boiling water under pressure through finely ground coffee beans. Espresso is generally thicker than coffee brewed by other methods, has a higher concentration of suspended and dissolved solids, and has crema on top (a foam with a creamy consistency).}, 
   image_url: 'https://goo.gl/c73hTL',
   price: 25.00)
+
+Product.create!(title: 'Mocha', 
+  description: %{Like a caffè latte, caffè mocha is based on espresso and hot milk, but with added chocolate, typically in the form of sweet cocoa powder, although many varieties use chocolate syrup. Mochas can contain dark or milk chocolate.}, 
+  image_url: 'http://goo.gl/4MIIwa',
+  price: 25.00)
+
+Product.create!(title: 'Latte', 
+  description: %{Coffee drink made with espresso and steamed milk.}, 
+  image_url: 'https://goo.gl/ur20RG',
+  price: 25.00)
